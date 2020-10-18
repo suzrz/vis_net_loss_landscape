98848fe3c8b084867e1029036792c68138b70f33
suzrz <you@example.com>
2020-10-18 22:50:32 +0200

    Graphs generating, now need to interpolate data

--- main.py
+++ main.py
@@ -1,31 +1,57 @@
-"""
-TODO
-    - two params
-    - jeden skalar z tensoru parametru
-
-"""
-import net
 import os
+import net
+import plot
 import torch
-import copy
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-
-
+import argparse
+import calc_loss
+import data_load
+import directions
+from torch import optim as optim
 from torch.optim.lr_scheduler import StepLR
 
 
-
-
-
-
 def main():
+    """PARSE ARGUMENTS"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    args = parser.parse_args()
 
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
 
+    """CREATE INSTANCE OF NEURAL NETWORK"""
+    model = net.Net().to(device)
+    # model.share_memory()
 
+    # If initialized state does not exist, create new one. Else skip.
+    if not os.path.isfile("init_state.pt"):
+        torch.save(model.state_dict(), "init_state.pt")
+        print("New init state saved.")
 
+    model.load_state_dict(torch.load("init_state.pt"))  # load initialized state from file
+
+    torch.manual_seed(1)  # set seed
+
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)  # set optimizer
+    scheduler = StepLR(optimizer, step_size=1, gamma=0.7)  # set scheduler
+
+    # check if exists trained model params
+    if not os.path.isfile("final_state.pt"):
+        print("Final state not found - beginning training")
+        for epoch in range(1, 2):  # here can be set number of epochs
+            net.train(model, data_load.train_loader, optimizer, device, epoch)
+            net.test(model, data_load.test_loader, device)
+            scheduler.step()
+            print("Finished epoch no. ", epoch)
+
+        torch.save(model.state_dict(), "final_state.pt")  # save final parameters of model
+
+    model.load_state_dict(torch.load("init_state.pt"))
+    dirs = directions.random_directions(model)  # get random directions
+    calc_loss.calculate_loss(model, dirs, device)  # calculate val loss and save it to surface file
+
+    plot.vis()
 
 
 if __name__ == "__main__":
--- plot.py
+++ plot.py
@@ -1,22 +1,58 @@
-import pickle
+import h5py
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-with open("v_loss_list.txt", "rb") as fd:
-    val_loss_list = pickle.load(fd)
 
-with open("t_loss_list.txt", "rb") as fd:
-    train_loss_list = pickle.load(fd)
+def vis():
 
-alpha = np.linspace(-0.25, 1.5, 13)
+    vmin = 0
+    vmax = 100
 
-# plot
-fig, axe = plt.subplots()
-axe.plot(alpha, val_loss_list, "x-", label="validation loss")
-axe.plot(alpha, train_loss_list, "o-", color="orange", label="train loss")  # not normalized! should be lower than validation loss but because it is measured on more samples, it looks worse
-axe.spines['right'].set_visible(False)
-axe.spines['top'].set_visible(False)
-plt.legend()
-plt.xlabel("alpha")
-plt.ylabel("loss")
-plt.show()
+    vlevel = 0.5
+    filename = "3D_surf.h5"
+    result_base = "./results/res_3D_surf"
+    surf_name = "val_loss"
+
+    with h5py.File(filename, 'r') as fd:
+        Z_LIMIT = 10
+
+        x = np.array(fd["xcoordinates"][:])
+        y = np.array(fd["ycoordinates"][:])
+
+        X, Y = np.meshgrid(x, y)
+        Z = np.array(fd[surf_name][:])
+
+        """3D"""
+        fig = plt.figure()
+        ax = Axes3D(fig)
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("f(x, y)")
+        ax.plot_surface(X, Y, Z, linewidth=0, antialiased=False)
+        plt.show()
+
+        """COUNTOURS"""
+        fig = plt.figure()
+        CS = plt.contour(X, Y, Z, cmap="summer",
+                         levels=np.arange(vmin, vmax, vlevel))
+        plt.clabel(CS, inline=1, fontsize=8)
+        #plt.savefig(result_base + '_' + surf_name + "_2D_contour" + ".pdf",
+        #            dpi=300, bbox_inches="tight", format="pdf")
+        plt.show()
+
+        """HEAT MAP"""
+        fig = plt.figure()
+        sns_plot = sns.heatmap(Z, cmap="viridis", cbar=True, vmin=vmin,
+                               vmax=vmax, xticklabels=False, yticklabels=False)
+        sns_plot.invert_yaxis()
+        #sns_plot.get_figure().savefig(result_base + '_' + surf_name + "_2D_heat.pdf",
+        #                              dpi=300, bbox_inches="tight", format="pdf")
+        plt.show()
+        """SAVE 3D"""
+        plt.figure()
+        ax = Axes3D(fig)
+        ax.plot_surface(X, Y, Z, linewidth=0, antialiased=True)
+        #fig.savefig(result_base + '_' + surf_name + "_3D_surface.pdf",
+        #            dpi=300, bbox_inches="tight", format="pdf")

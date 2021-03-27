fixed single parameter interpolation and distance calculation

interpolate.py
@@ -34,9 +34,9 @@ class Interpolator:
 
     def calc_distance(self, layer, idxs=None):
         if not idxs:
-            return torch.mean(torch.sqrt(torch.pow(self.theta_f[layer], 2) + torch.pow(self.theta_i[layer], 2)))
+            return torch.dist(self.theta_f[layer], self.theta_i[layer])
         else:
-            return torch.mean(torch.sqrt(torch.pow(self.theta_f[layer][idxs], 2) + torch.pow(self.theta_i[layer][idxs], 2)))
+            return torch.dist(self.theta_f[layer][idxs], self.theta_i[layer][idxs])
 
     def calc_theta_single(self, layer, idxs, alpha):
         logging.debug("[interpolate]: Calculating value of: {} {} for alpha = {}".format(
@@ -104,7 +104,6 @@ class Interpolator:
             self.model.load_state_dict(self.theta_f)
             for alpha_act in self.alpha:
                 self.calc_theta_single(layer + ".weight", idxs, alpha_act)
-                self.calc_theta_single(layer + ".bias", idxs, alpha_act)
 
                 self.model.load_state_dict(self.theta)
                 logging.debug("[interpolator.single_acc_vloss]: Getting validation loss "
@@ -255,3 +254,7 @@ class Interpolator:
         np.savetxt(sf_acc_path, acc)
 
         return loss, acc
+
+    def print_theta(self, layer, idxs):
+        layer = layer + ".weight"
+        print(self.theta[layer][idxs])

main.py
@@ -151,12 +151,13 @@ def main():
     model2 = copy.deepcopy(model)
     interpolate = Interpolator(model, device, alpha, final_state, init_state)  # Create interpolator
 
-    #interpolate.single_acc_vloss(test_loader, args.layer, list(map(int, args.idxs)))  # examine parameter
+    #interpolate.print_theta(args.layer, (0, 0))
+    interpolate.single_acc_vloss(test_loader, args.layer, (65, 367))  # examine parameter
     model.load_state_dict(torch.load(final_state))
     #interpolate.vec_acc_vlos(test_loader, args.layer, trained=args.trained)
     #interpolate.rand_dirs(test_loader)
     #plot.surface3d_rand_dirs()
-    interpolate.interpolate_all(test_loader)
+    #interpolate.interpolate_all(test_loader)
 
 
     #plot.plot_single(alpha, "conv1", True)

plot.py
@@ -262,7 +262,7 @@ def plot_vec_in_one(x, metric, opacity_dict):
 
 def plot_vec_all_la(x, opacity_dict):
     files = os.listdir(vec)
-    fig, (ax, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+    fig, (ax, ax2) = plt.subplots(1, 2, figsize=(8, 3))
 
     #ax = fig.add_subplot()
     ax.spines["right"].set_visible(False)
@@ -288,7 +288,7 @@ def plot_vec_all_la(x, opacity_dict):
     ax.plot(x, np.loadtxt(loss_path), label="all", color=color_trained, linewidth=1)
     ax2.plot(x, np.loadtxt(acc_path), color=color_trained, linewidth=1)
 
-    fig.legend(loc="lower center", ncol=6, mode="expand")
+    fig.legend()
     fig.subplots_adjust(bottom=0.17)
     plt.savefig("{}_{}.pdf".format(vec_img, "all_la"), format="pdf")
     plt.show()
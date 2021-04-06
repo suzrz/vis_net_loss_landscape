b6a26fbd2d2cd59dadfb4576f177fb3253fa8723
suzrz <nemcova.silva@gmail.com>
2021-04-06 10:37:50 +0200

    refact: added logging and execution of quadratic interpolation

--- interpolate.py
+++ interpolate.py
@@ -87,14 +87,17 @@ class Interpolator:
         ydata = np.array([start[1], mid[1], end[1]])
         logger.debug(f"YDATA: {ydata}")
         #self.fit_params, self.p_cov = scipy.optimize.curve_fit(parabola, xdata, ydata)
+
         poly = scipy.interpolate.lagrange(xdata, ydata)
+
         self.fit_params = Polynomial(poly).coef
+        logger.debug(f"Coefficients: {self.fit_params}")
 
         self.theta[layer][idxs] = torch.tensor(((1.0 - alpha)*self.fit_params[0]**2 + alpha*self.fit_params[1] +
                                                    self.fit_params[2]) / 100).to(self.device)
-
         logger.debug(f"Modified theta:\n"
                      f"{self.theta[layer][idxs]}")
+
 
     def calc_theta_vec(self, layer, alpha):
         """
@@ -233,14 +236,14 @@ class Interpolator:
             acc_list = []
 
             start_p = self.theta_i[layer + ".weight"][idxs]
-            mid_p = copy.deepcopy(torch.load(Path(os.path.join(results, "state_4"))))[layer + ".weight"][idxs]
+            mid_p = copy.deepcopy(torch.load(Path(os.path.join(results, "state_7"))))[layer + ".weight"][idxs]
             end_p = self.theta_f[layer + ".weight"][idxs]
             logger.debug(f"Start: {start_p}\n"
                          f"Mid: {mid_p}\n"
                          f"End: {end_p}")
 
             start_loss = np.loadtxt(actual_loss_path)[0]
-            mid_loss = np.loadtxt(actual_loss_path)[2]
+            mid_loss = np.loadtxt(actual_loss_path)[6]
             end_loss = np.loadtxt(actual_loss_path)[-1]
             logger.debug(f"Start loss: {start_loss}\n"
                          f"Mid loss: {mid_loss}\n"
